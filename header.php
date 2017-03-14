d6ce6666fd5c70a412b39c0fadde8eb186fb9538
Joakim <joakim@idrottskoll.se>
2017-03-14 08:27:08 +0100

    writing and moved text

--- header.php
+++ header.php
@@ -13,29 +13,26 @@
 </head>
 <body>
 
+  <?php
+  // print '<pre>';
+  // print_r(get_pages());
+  ?>
 
   <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="<?php echo get_home_url(); ?>">Navbar</a>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
 
         <?php if (have_posts()): ?>
-          <?php while (have_posts()): the_post(); ?>
-
-
+          <?php foreach (get_pages() as $page): ?>
             <li class="nav-item">
-              <a class="nav-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              <a class="nav-link" href="<?php print $page->guid; ?>"><?php print $page->post_title; ?></a>
             </li>
-
-
-          <?php endwhile; ?>
+            <?php endforeach; ?>
         <?php endif; ?>
-
-
-
 
       </ul>
     </div>
